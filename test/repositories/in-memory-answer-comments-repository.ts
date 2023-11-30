d5d5674e8a6c97ce13a8f1632263d3fca3aa3aea
Arthur Pedroti <arthurpedroti@gmail.com>
2023-11-30 15:39:47 -0300

    use case: fetch answer comments

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,8 +1,9 @@
-import { AnswersCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
 export class InMemoryAnswerCommentsRepository
-  implements AnswersCommentsRepository
+  implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
@@ -14,6 +15,14 @@ export class InMemoryAnswerCommentsRepository
     }
 
     return answerComment
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async create(answerComment: AnswerComment) {
